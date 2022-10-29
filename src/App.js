a8f0db06545a3ed95e18fc61db2a11864518a2ab
AJ Glodowski <ajglodo@gmail.com>
2022-10-29 17:31:16 -0400

    Adding start to React app with Tailwind CSS and Material UI

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './input.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-2xl underline">
+        Tourney Predictor
+      </h1>
+      <Button variant="contained">Test Button</Button>
+    </div>
+  );
+}
+
+export default App;
